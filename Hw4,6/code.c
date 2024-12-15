cbacf56b71b70bb767d22a4b192e584c604d3518
Anuphat <anuphatkk@gmail.com>
2024-12-15 22:41:14 +0700

    Create code.c

---
+++ Hw4,6/code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char employeeID[11] ;
+    int workingHours ;
+    float salaryPerHour, totalSalary ;
+
+    printf( "Input the Employees ID(Max. 10 chars): \n" ) ;
+    scanf( "%s", employeeID ) ;
+
+    printf( "Input the working hrs: \n" ) ;
+    scanf( "%d", &workingHours ) ;
+
+    printf( "Salary amount/hr: \n" ) ;
+    scanf( "%f", &salaryPerHour ) ;
+
+    totalSalary = workingHours * salaryPerHour ;
+
+    printf( "Employees ID = %s\n", employeeID ) ;
+    printf( "Salary = U$ %,.2f\n", totalSalary ) ;
+
+    return 0 ;
+}//end main
