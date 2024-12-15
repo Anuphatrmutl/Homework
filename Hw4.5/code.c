a6781d0820a781c3e68077cea78d3c0fe4175550
Anuphat <anuphatkk@gmail.com>
2024-12-15 22:34:54 +0700

    Create code.c

---
+++ Hw4.5/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int days ;
+    long seconds ;
+
+    printf( "Input Days : \n" ) ;
+    scanf( "%d", &days ) ;
+
+    seconds = days * 24 * 60 * 60 ;
+
+    printf( "%d days = %ld seconds\n", days, seconds ) ;
+
+    return 0 ;
+}//end main
